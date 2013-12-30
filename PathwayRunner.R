943c38feeb27018527fa80493088be85f1f2c0df
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2013-12-29 20:40:37 -0600

    Complete working prototype of grouping gene sets of interest by their pathway similarity

--- PathwayRunner.R
+++ PathwayRunner.R
@@ -1,54 +1,33 @@
-# Fooling around with reactome.db functionality
-xx <- as.list(reactomeEXTID2PATHID) # maps Entrez Gene identiﬁers to Reactome pathway identiﬁers
-xx[1]
-xx<-as.list(reactomeGO2REACTOMEID) # maps GO identiﬁers to Reactome database identiﬁers
-xx[1]
-xx<-as.list(reactomePATHID2EXTID) # maps Reactome pathway identiﬁers to Entrez Gene identiﬁers
-xx[1]
-xx<-as.list(reactomePATHID2NAME) # maps Reactome pathway identiﬁers to pathway names used by Reactome for various pathways
-xx[1]
-xx<-as.list(reactomePATHNAME2ID) # maps Reactome pathway names to pathway identiﬁers used by Reactome for various pathways
-xx[1]
-xx<-as.list(reactomeREACTOMEID2GO) # maps Reactome database identiﬁers to Gene Ontoloty (GO) identiﬁers
-xx[1]
-reactomeMAPCOUNTS # provides the "map count" (i.e. the count of mapped keys) for each map in package reactome.db
+# PathwayRunner computes enrichment of gene sets of interest in pre-defined gene sets using Fisher's exact test
+# Example shows enrichment of 188 KEGG pathways (gene sets of interest) in 10,469 reactomeDb pathways (pre-defined gene sets)
+# Prerequisite 1: Create gene.list, each element containing gene sets of interest. Can contain a single gene list. Example populates gene.list with gene sets defining KEGG pathways
+# Prerequisite 2: Select pre-defined gene sets. Example uses gene sets defining reactomeDb pathways
+source("utils.R") # functions for gene ID conversion
 
-#
-# PathwayRunner computed enrichment of gene set(s) in all pathways using hypergeometric test
-# Gene sets' pathway enrichment profiles can then be pairwise correlated, 
-# and correlation coefficients clustered and visualized, showing relationships among gene sets
-# by their pathway connectivity
-# 
-# --------------------
-# PathwayRunner begins
-# --------------------
+# Prerequisite 1: Create gene.list, each element containing gene sets of interest.
+library(GSA)
+geneset.obj<-GSA.read.gmt("data//msigdb.gmt") # Read all MSigDb gene sets
+gene.list<-geneset.obj$genesets[grep("KEGG", geneset.obj$geneset.names)] # Get gene names for 186 KEGG pathways
+gene.list<-lapply(gene.list, name2Entrez) # Convert gene names to EntrezIDs
 
-# ToDo: Add conversion of UniProt IDs (or any gene IDs) to EntreZ IDs, used by reactome.db
-
+# Prerequisite 2: Select pre-defined gene sets.
 library('reactome.db')
-pathway.count<-length(names(as.list(reactomePATHID2EXTID))) # Total number of pathways (9,381)
-pathid.exid<-as.list(reactomePATHID2EXTID) # Each element is a pathID, containing list of genes in that pathway. Used to test input gene set(s) against each pathway in this pathway list
-total.gene.count<-length(names(as.list(reactomeEXTID2PATHID))) # Total number of genes (29,492)
+pathway.count<-length(names(as.list(reactomePATHID2EXTID))) # Total number of pre-defined gene sets
+pathid.exid<-as.list(reactomePATHID2EXTID) # Each element is a pathID, contains a list of genes in that pathway
+total.gene.count<-length(names(as.list(reactomeEXTID2PATHID))) # Total number of genes
 
-# Create list of input gene set(s), to be tested for the enrichments against each pathway
-test.count<-15 # For testing purposes, simply use genes from first test.count pathways
-xx <- as.list(reactomePATHID2EXTID) # maps Entrez Gene identiﬁers to Reactome pathway identiﬁers
-gene.list<-list() # Initialize list containing gene set(s) to be tested for enrichment
-for (i in 1:test.count) { # Populate this list with genes 
-  gene.list[[length(gene.list)+1]] <- list(xx[[i]]) # Incrementally append gene sets to the list
-}
+# mtx.pval will contain -log10-transformed enrichment p-values, with "-" added to indicate depletion
+mtx.pval<-matrix(numeric(0), pathway.count, length(gene.list)) # Initialize empty [nxm] matrix, which will contain enrichment results of testing gene sets of interest [m] vs. pre-defined gene sets
+colnames(mtx.pval)<-geneset.obj$geneset.names[grep("KEGG", geneset.obj$geneset.names)] # Set column names as the names of the gene sets of interest
 
-# Initialize empty matrix, which will contain enrichment results of testing gene set(s) [columns]
-# vs. pathways [rows]
-mtx.pval<-matrix(numeric(0), test.count, length(gene.list))
-
+Sys.time() # Starting time
 # Now, the actual enrichment analysis
-for (i in 1:length(gene.list)){ # Test gene sets one by one
-  for (j in 1:test.count) { # Against all pathways
-    pathway.length<-length(pathid.exid[[j]]) # Number of genes in the current pathway
-    pathway.genes<-as.character(pathid.exid[[j]]) # Gene IDs in the current pathway
-    genes.count<-length(unlist(gene.list[[i]])) # Number of input genes in the current set
-    genes.in.pathway<-length(intersect(unlist(gene.list[[i]]),pathway.genes)) # Number of input genes in the current set present in the current pathway
+for (i in 1:length(gene.list)){ # Test gene sets of interest one by one
+  for (j in 1:pathway.count) { # Against each pre-defined gene set
+    pathway.length<-length(pathid.exid[[j]]) # Number of genes in the current pre-defined gene set
+    pathway.genes<-as.character(pathid.exid[[j]]) # Gene IDs in the current  pre-defined gene set
+    genes.count<-length(unlist(gene.list[[i]])) # Number of  genes in the current gene set of interest
+    genes.in.pathway<-length(intersect(unlist(gene.list[[i]]), pathway.genes)) # Overlapping genes
     # 2 x 2 contingency table for hypergeometric test.
     # Example from http://mengnote.blogspot.com/2012/12/calculate-correct-hypergeometric-p.html
     #
@@ -68,5 +47,43 @@ for (i in 1:length(gene.list)){ # Test gene sets one by one
     if (p.value$estimate < 1) mtx.pval[j, i] <- -mtx.pval[j, i] # If odds ratio < 1, then we have depletion, add "-" to highlight this fact
   }
 }
+Sys.time() # Ending time
 
-# ToDo: Add correlation, clustering and visualization of pathway connectivity
+
+# Clustering and visualization of pathway connectivity
+mtx.cor<-cor(mtx.pval) #, use="pairwise.complete.obs")
+library(gplots)
+par(mar=c(10,6,6,5),oma=c(2,2,2,2)) #Make right and bottom margins larger
+color<-colorRampPalette(c("blue","yellow"))
+dist.method<-"euclidean"  # "euclidean", "maximum", "manhattan", "canberra", "binary" or "minkowski"
+hclust.method<-"average" # "ward", "single", "complete", "average", "mcquitty", "median" or "centroid"
+#Maximum distance seems to best separate correlations. Centroid linkage further sharpen the separation
+h<-heatmap.2(as.matrix(mtx.cor),trace="none",density.info="none", col=color,distfun=function(x){dist(x,method=dist.method)}, hclustfun=function(x){hclust(x,method=hclust.method)},cexRow=0.8,cexCol=0.8)
+
+write.table(mtx.cor, "data//KEGG_vs_Reactome//KEGG_vs_Reactome.txt", sep="\t")
+
+# # Experimenting with clustering/visualization with missing values
+# # http://stat.ethz.ch/R-manual/R-devel/library/cluster/html/00Index.html
+# library(cluster)
+# plot(agnes(mtx.cor))
+# library(ggplot2)
+# x<-agnes(mtx.cor)
+# y<-agnes(t(mtx.cor))
+# # http://stats.stackexchange.com/questions/9050/how-to-display-a-matrix-of-correlations-with-missing-entries
+# ggfluctuation(as.table(mtx.cor[x$order,y$order]), type="color") + labs(x="", y="") +
+#   scale_fill_gradient(low = "blue",  high = "red")
+
+# # Fooling around with reactome.db functionality
+# xx <- as.list(reactomeEXTID2PATHID) # maps Entrez Gene identiﬁers to Reactome pathway identiﬁers
+# xx[1]
+# xx<-as.list(reactomeGO2REACTOMEID) # maps GO identiﬁers to Reactome database identiﬁers
+# xx[1]
+# xx<-as.list(reactomePATHID2EXTID) # maps Reactome pathway identiﬁers to Entrez Gene identiﬁers
+# xx[1]
+# xx<-as.list(reactomePATHID2NAME) # maps Reactome pathway identiﬁers to pathway names used by Reactome for various pathways
+# xx[1]
+# xx<-as.list(reactomePATHNAME2ID) # maps Reactome pathway names to pathway identiﬁers used by Reactome for various pathways
+# xx[1]
+# xx<-as.list(reactomeREACTOMEID2GO) # maps Reactome database identiﬁers to Gene Ontoloty (GO) identiﬁers
+# xx[1]
+# reactomeMAPCOUNTS # provides the "map count" (i.e. the count of mapped keys) for each map in package reactome.db
---
+++ utils.R
@@ -0,0 +1,20 @@
+# BiomaRt: converting gene identifiers
+library(biomaRt)
+mart<-useMart("ensembl", dataset="hsapiens_gene_ensembl")
+
+name2Entrez<-function(names){ # Gene name to Entrez
+  as.vector(as.matrix(getBM(attributes='entrezgene', filters='wikigene_name', values=names, mart=mart, uniqueRows=T)))
+}
+
+uniprot2Entrez<-function(names){ # UniProt to Entrez
+  as.vector(as.matrix(getBM(attributes='entrezgene', filters='uniprot_swissprot_accession', values=names, mart=mart, uniqueRows=T)))
+}
+# 146                      uniprot_sptrembl               UniProt/TrEMBL Accession(s) [e.g. A2MYD1]
+# 147                     uniprot_swissprot              UniProt/Swissprot ID(s) [e.g. GAGE4_HUMAN]
+# 148           uniprot_swissprot_accession            UniProt/Swissprot Accession(s) [e.g. Q13068]
+# 150                      uniprot_genename                      UniProt Genename ID(s) [e.g. V4-4]
+# 152      uniprot_genename_transcript_name Uniprot Genename Transcript Name ID(s) [e.g. SEPT1-202]
+
+Entrez2name<-function(names){ # Entrez to gene name
+  as.vector(as.matrix(getBM(attributes='wikigene_name', filters='entrezgene', values=names, mart=mart, uniqueRows=T)))
+}
